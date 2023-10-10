3cac5e4fcd12a1f4f9708ccdfee04bb7a14fe8f5
verzhik <verzhik11@gmail.com>
2023-10-10 22:13:29 +0300

    Pull request

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,5 @@ public class Main {
         HashSet<String> stringSet = new LinkedHashSet<>(Arrays.asList(strings));
         String result = String.join(", ", stringSet);
         System.out.println(result);
-
     }
 }
