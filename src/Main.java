cd2f3261702e1e5403bc7999fdd747e07d36ebbf
verzhik <verzhik11@gmail.com>
2023-10-10 22:12:07 +0300

    Решение задачи

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] strings = {"Sun", "Snow", "Rain", "Sun", "Frost", "Snow", "Tornado", "Dream", "Sun"};
+        HashSet<String> stringSet = new LinkedHashSet<>(Arrays.asList(strings));
+        String result = String.join(", ", stringSet);
+        System.out.println(result);
+
+    }
+}
